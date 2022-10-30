ab852435455975ba8a7d0d8c22ee82b63aaa9ec6
munuu <munuu@DESKTOP-IRAT9DQ>
2022-10-30 19:42:08 +0530

    Add project files.

---
+++ prjRom/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace prjRom.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
